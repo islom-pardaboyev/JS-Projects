7a4c76f19da9e9028314623218a34fd550a0d978
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-02-04 20:35:31 +0500

    Updated File

--- index.js
+++ index.js
@@ -39,4 +39,36 @@ const elBikeTime = document.querySelector(".bike-time")
 const elCarTime = document.querySelector(".car-time")
 const elAirplaneTime = document.querySelector(".airplane-time")
 
-console.log(elForm2, elDistanceInput, elWalkTime, elBikeTime, elCarTime, elAirplaneTime);
+const piyoda = 3.60
+const velosiped = 20.10
+const mashina = 70.00
+const samolyot = 800.00
+
+elForm2.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const distance = parseFloat(elDistanceInput.value.trim(), 10);
+
+    if(distance > 0){
+        const walkTime = distance / piyoda;
+        const walHour = parseInt(walkTime, 10);
+        const walkMinute = Math.round((parseFloat(walkTime.toFixed(2), 10) - walHour) * 60)
+
+        const bikeTime = distance / velosiped;
+        const bikeHour = parseInt(bikeTime, 10)
+        const bikeMinute = Math.round((parseFloat(bikeTime.toFixed(2), 10) - bikeHour) * 60)
+
+        const carTime = distance / mashina
+        const carHour = parseInt(carTime, 10)
+        const carMinute = Math.round((parseFloat(carTime.toFixed(2), 10) - carHour) * 60)
+
+        const planeTime = distance / samolyot
+        const planeHour = parseInt(planeTime, 10)
+        const planeMinute = Math.round((parseFloat(planeTime.toFixed(2), 10) - planeHour) * 60)
+
+        elWalkTime.textContent = `${walHour} hours, ${walkMinute} min`;
+        elBikeTime.textContent = `${bikeHour} hours, ${bikeMinute} min`;
+        elCarTime.textContent = `${carHour} hours, ${carMinute} min`;
+        elAirplaneTime.textContent = `${planeHour} hours, ${planeMinute} min`;
+    }
+})
